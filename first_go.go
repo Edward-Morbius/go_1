27cb5b2e6bec49ed0a0402434b52020b48abcfcd
Edward Morbius <pararaum@gmail.com>
2017-07-13 09:35:13 +0200

    Read directory.

--- first_go.go
+++ first_go.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 	fmt.Println("First go.")
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+
+		for num, file := range files {
+			fmt.Printf("%04x $%08X %52s\n", num, file.Size(), file.Name())
+		}
+	}
 }
